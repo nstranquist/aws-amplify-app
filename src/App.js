2affd71de3d2ba649e02f824db5dfbbd24328791
Nico Stranquist <nstranquist@gmail.com>
2022-01-27 12:01:21 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Auth } from 'aws-amplify'
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
@@ -7,19 +10,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from V2, with Auth!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello from V2
-        </a>
       </header>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="App">
+            <p>
+              Hey {user.username}, welcome to my channel, with auth!
+            </p>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
