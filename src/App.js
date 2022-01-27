f8ee157ad88f219ef74b368a3cb3b3862190658b
Nico Stranquist <nstranquist@gmail.com>
2022-01-27 10:33:16 -0600

    test redeploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello from V2
         </a>
       </header>
     </div>
